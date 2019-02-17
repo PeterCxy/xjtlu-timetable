e8eaf1e0cedae8bd04e7074d99b80ec3732a35d4
Peter Cai <peter@typeblog.net>
2019-02-17 19:25:32 +0800

    update for new semester :)

--- src/constants.rs
+++ src/constants.rs
@@ -7,14 +7,15 @@
 /*
  * Duration of the current semester.
  */
-pub const SEMESTER_START: (u32, u32) = (9, 9); // The day before the semester starts
-pub const SEMESTER_END: (u32, u32) = (12, 21); // The day the semester ends
+pub const SEMESTER_START: (u32, u32) = (2, 17); // The day before the semester starts
+pub const SEMESTER_END: (u32, u32) = (5, 25); // The day the semester ends
 
 /*
  * Days on which we will not have any classes
  */
 pub const VACATION_DAYS: &'static [(u32, u32)] = &[
-    (9, 24)
+    (4, 5),
+    (5, 1)
 ];
 
 /*
@@ -22,5 +23,4 @@ pub const VACATION_DAYS: &'static [(u32, u32)] = &[
  */
 pub const VACATION_WEEKS: &'static [(u32, u32)] = &[
     // Nothing here yet.
-    (10, 1)
 ];
